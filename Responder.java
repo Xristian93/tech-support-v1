80e721e740ff5cf0b05179dfc0a220c466bbe3ac
Xristian93 <cristianfuente72@gmail.com>
2018-02-07 13:23:12 +0100

    Generacion de respuestas aleatorias en metodo generateResponse

--- Responder.java
+++ Responder.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +10,21 @@
  */
 public class Responder
 {
+    private Random aleatorio;
+    private ArrayList<String> listaRespuestas;
+    
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        listaRespuestas = new ArrayList<String>();
+        listaRespuestas.add("Muy interesante");
+        listaRespuestas.add("No lo entiendo");
+        listaRespuestas.add("Alguna duda mas?");
+        listaRespuestas.add("Lo siento, no puedo ayudarle");
+        listaRespuestas.add("Buena idea");
     }
 
     /**
@@ -20,6 +33,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int posicion = aleatorio.nextInt(listaRespuestas.size());
+        return listaRespuestas.get(posicion);
     }
 }
