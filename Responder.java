84be3b12c57c3ef4ed37ed6a38ff8711a12dd1b2
Xristian93 <cristianfuente72@gmail.com>
2018-02-15 20:07:12 +0100

    Modificadas clases Responder y SupportSystem para que respondan de acuerdo a una sola palabra

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * The responder class represents a response generator object.
@@ -12,7 +13,8 @@ public class Responder
 {
     private Random aleatorio;
     private ArrayList<String> listaRespuestas;
-    
+    private HashMap<String, String> responseMap;
+
     /**
      * Construct a Responder - nothing to do
      */
@@ -25,13 +27,38 @@ public class Responder
         listaRespuestas.add("Alguna duda mas?");
         listaRespuestas.add("Lo siento, no puedo ayudarle");
         listaRespuestas.add("Buena idea");
+        responseMap = new HashMap<String, String>();
+        fillResponseMap();
     }
 
     /**
-     * Generate a response.
+     * Introducir en el mapa de respuestas las palabras claves y sus respuestas
+     */
+    public void fillResponseMap(){
+        responseMap.put("lento","Creo que tienes un problema con el hardware de tu equipo");
+        responseMap.put("error","Todo software tiene algun pequeño error, no se preocupe");
+        responseMap.put("problema","Ese problema es muy facil de solucionar, reinicie su equipo");
+        responseMap.put("grafica","Su tarjeta grafica esta muy anticuada, pruebe a cambiarla");
+    }
+
+    /**
+     * Generate a response
+     */
+    public String generateResponse (String word){
+        String response = responseMap.get(word);
+        if (response != null){
+            return response;
+        }
+        else {
+            return pickDefaultResponse();
+        }
+    }
+
+    /**
+     * Generate a default response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String pickDefaultResponse()
     {
         int posicion = aleatorio.nextInt(listaRespuestas.size());
         return listaRespuestas.get(posicion);
